bd9fd3e8f1da149ffd3bae36d13f403db13f5e1e
Asaf Aviv <asafaviv89@gmail.com>
2019-06-16 19:49:33 +0300

    structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
-import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+
+import 'normalize.css';
+import './App.sass';
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <p>Hello</p>
-    </header>
-  </div>
+  <main className="App">
+    <Navbar />
+    <About />
+    <Projects />
+    <Skills />
+    <Contact />
+  </main>
 );
 
 export default hot(App);
