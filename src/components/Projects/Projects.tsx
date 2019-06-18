456b02da95809b061ac98ecbda49973cd335fab3
Asaf Aviv <asafaviv89@gmail.com>
2019-06-18 22:19:06 +0300

    Projects

---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../Container';
+
+import './Projects.sass';
+
+const Projects: React.FC = () => (
+  <section className="projects">
+    <Container />
+  </section>
+);
+
+export default Projects;
