ed79f3887306702ebcd87c3e15ca11b5df1dc09e
Asaf Aviv <asafaviv89@gmail.com>
2019-06-20 22:18:09 +0300

    Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../Container';
+
+import './Footer.sass';
+
+const Footer: React.FC = () => (
+  <footer className="footer">
+    <Container>
+      <h3>Footer</h3>
+    </Container>
+  </footer>
+);
+
+export default Footer;
