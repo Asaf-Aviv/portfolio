dc05263f8be8d876cca6f63595b42e4854c0e9e6
Asaf Aviv <asafaviv89@gmail.com>
2019-06-20 22:18:05 +0300

    Projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,12 +1,65 @@
 import React from 'react';
-import Container from '../Container';
+import Section from '../Section';
+import ProjectCard from '../ProjectCard';
+import BlogglyScreenShot from '../../assets/projects screenshots/blogggly.png';
+import BlogglyDarkScreenShot from '../../assets/projects screenshots/blogggly-dark.png';
 
 import './Projects.sass';
 
+interface ProjectCardProps {
+  name: string;
+  description: string;
+  screenshot: string;
+  darkScreenshot?: string;
+  backend: string[];
+  frontend: string[];
+  db: string[];
+  href: string;
+  github: string;
+}
+
+const Blogggly = {
+  name: 'Blogggly',
+  description: 'Platform for bloggers',
+  screenshot: BlogglyScreenShot,
+  darkScreenshot: BlogglyDarkScreenShot,
+  backend: ['Node.js', 'GraphQL'],
+  db: ['MongoDB'],
+  frontend: ['React', 'SASS', 'WebSockets'],
+  href: 'https://blogggly.com/',
+  github: 'https://github.com/Asaf-Aviv/blogggly',
+};
+
+const Blogggly2 = {
+  name: 'Blogggly2',
+  description: 'Platform for bloggers',
+  screenshot: BlogglyScreenShot,
+  darkScreenshot: BlogglyDarkScreenShot,
+  backend: ['Node.js', 'GraphQL'],
+  db: ['MongoDB'],
+  frontend: ['React', 'SASS', 'WebSockets'],
+  href: 'https://blogggly.com/',
+  github: 'https://github.com/Asaf-Aviv/blogggly',
+};
+
+const projects: ProjectCardProps[] = [Blogggly, Blogggly2];
+
+const ProjectsList: React.FC<React.ReactNode> = ({ children }) => (
+  <ul className="projects__list">{children}</ul>
+);
+
+const renderProjectCard = (project: ProjectCardProps) => (
+  <li className="projects__list-item" key={project.name}>
+    <ProjectCard {...project} />
+  </li>
+);
+
 const Projects: React.FC = () => (
-  <section className="projects">
-    <Container />
-  </section>
+  <Section id="projects" classes="projects" title="Projects">
+    <ProjectsList>
+      {projects.map(renderProjectCard)}
+    </ProjectsList>
+  </Section>
 );
 
 export default Projects;
