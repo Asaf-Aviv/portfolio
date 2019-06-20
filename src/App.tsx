758e3e92082a44262d6fb9e98e3ecce3bded60f3
Asaf Aviv <asafaviv89@gmail.com>
2019-06-20 22:17:02 +0300

    added togglers state, useActionScrollLinks hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { hot } from 'react-hot-loader/root';
 import Navbar from './components/Navbar';
 import About from './components/About';
@@ -6,22 +6,39 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 import Canvas from './components/Canvas';
+import Footer from './components/Footer';
 import LogosProvider from './components/Canvas/LogoLoaderProvider';
+import useDarkMode from './hooks/useDarkMode';
+import useActiveScrollLinks from './hooks/useActiveScrollLinks';
 
 import 'normalize.css';
 import './App.sass';
 
-const App: React.FC = () => (
-  <main className="App">
-    <LogosProvider>
-      <Canvas />
-    </LogosProvider>
-    <Navbar />
-    <About />
-    <Projects />
-    <Skills />
-    <Contact />
-  </main>
-);
+const App: React.FC = () => {
+  const [showAnimations, setShowAnimations] = useState(true);
+  const [isDarkMode, toggleDarkMode] = useDarkMode();
+  useActiveScrollLinks();
+
+  return (
+    <main className="App">
+      {showAnimations && (
+        <LogosProvider>
+          <Canvas />
+        </LogosProvider>
+      )}
+      <Navbar
+        isDarkMode={isDarkMode}
+        toggleDarkMode={toggleDarkMode}
+        showAnimations={showAnimations}
+        toggleAnimations={() => setShowAnimations(prevState => !prevState)}
+      />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
 
 export default hot(App);
