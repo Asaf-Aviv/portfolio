d8a0dfcb6a2910019f68436b4988ba92cfa2ff2a
Asaf Aviv <asafaviv89@gmail.com>
2019-06-21 14:17:42 +0300

    MyLinks

---
+++ src/components/MyLinks/MyLinks.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import GitHubLink from '../GitHubLink';
+import { ReactComponent as StackOverflowLogo } from '../../assets/logos/stackoverflow.svg';
+
+const MyLinks: React.FC = () => (
+  <div className="my-links">
+    <GitHubLink classes="my-links__link" href="https://github.com/Asaf-Aviv" />
+    <a
+      className="my-links__link"
+      href="https://stackoverflow.com/users/8605960/asaf-aviv"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <StackOverflowLogo />
+    </a>
+  </div>
+);
+
+export default MyLinks;
