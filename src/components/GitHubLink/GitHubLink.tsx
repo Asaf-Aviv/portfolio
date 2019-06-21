f7392dedf19ddf7652bde4775de7d7f3862c69e3
Asaf Aviv <asafaviv89@gmail.com>
2019-06-21 16:07:14 +0300

    test

--- src/components/GitHubLink/GitHubLink.tsx
+++ src/components/GitHubLink/GitHubLink.tsx
@@ -14,6 +14,7 @@ const GitHubLink: React.FC<GitHubLinkProps> = ({ href, classes = '' }) => (
     rel="noopener noreferrer"
   >
     <GithubLogo className="github-logo" />
+    <span>hello test1111</span>
   </a>
 );
 
