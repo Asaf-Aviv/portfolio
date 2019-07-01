7ec863d56f47d06874838a39e3411533eeffaef3
Asaf Aviv <asafaviv89@gmail.com>
2019-07-01 16:13:26 +0300

    fixed contact form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -34,48 +34,40 @@ Input.defaultProps = {
   autocomplete: 'false',
 };
 
-const Contact: React.FC = () => {
-  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // TODO: Send POST request to formspree manually
-  };
-
-  return (
-    <Section id="contact">
-      <ContentCard>
-        <div className="contact__container">
-          <div className="contact__form-container">
-            <form
-              action="https://formspree.io/asafaviv89@gmail.com"
-              method="POST"
-              className="contact__form"
-              onSubmit={onSubmit}
-            >
-              <h3 className="contact__form-title">Send me a message</h3>
-              <Input type="text" name="name" placeholder="Name" />
-              <Input type="email" name="email" placeholder="Email" autocomplete="true" />
-              <textarea className="textarea" name="message" placeholder="Message" required rows={6} />
-              <button type="submit" className="contact__form-submit">
+const Contact: React.FC = () => (
+  <Section id="contact">
+    <ContentCard>
+      <div className="contact__container">
+        <div className="contact__form-container">
+          <form
+            action="https://formspree.io/asafaviv89@gmail.com"
+            method="POST"
+            className="contact__form"
+          >
+            <h3 className="contact__form-title">Send me a message</h3>
+            <Input type="text" name="name" placeholder="Name" />
+            <Input type="email" name="email" placeholder="Email" autocomplete="true" />
+            <textarea className="textarea" name="message" placeholder="Message" required rows={6} />
+            <button type="submit" className="contact__form-submit">
                 Send
-                <EmailIcon />
-              </button>
-            </form>
+              <EmailIcon />
+            </button>
+          </form>
+        </div>
+        <div className="contact__details">
+          <h3 className="contact__details-title">Contact Information</h3>
+          <div className="contact__details-item">
+            <span>(972) 544 502 505</span>
           </div>
-          <div className="contact__details">
-            <h3 className="contact__details-title">Contact Information</h3>
-            <div className="contact__details-item">
-              <span>(972) 544 502 505</span>
-            </div>
-            <div className="contact__details-item">
-              <a className="email__link" href="mailto:asafaviv89@gmail.com">
+          <div className="contact__details-item">
+            <a className="email__link" href="mailto:asafaviv89@gmail.com">
                 asafaviv89@gmail.com
-              </a>
-            </div>
+            </a>
           </div>
         </div>
-      </ContentCard>
-    </Section>
-  );
-};
+      </div>
+    </ContentCard>
+  </Section>
+);
 
 export default Contact;
